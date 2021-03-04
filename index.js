33276fbf38d27e51691e2e7dff4afcc282c06fbd
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-03-04 14:20:49 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let http = require('http')
+let url = require('url')
+let fs = require('fs')
+
+http.createServer((req, res) => {
+	let q = url.parse(req.url, true);
+	let filename = `.${q.pathname}.html`
+	fs.readFile(filename, (err, data) => {  
+		if (err) {
+			fs.readFile("./404.html", (err, data) =>{
+				res.writeHead(404, {'Content-Type': 'text/html'})
+				res.write(data);
+				return res.end()
+			}); 
+		} else {
+			res.writeHead(200, {'Content-Type': 'text/html'})
+			res.write(data);
+			return res.end();
+		}
+	})
+}).listen(8080);
