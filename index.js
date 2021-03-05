f0ad6c77f0550484f7b1b113002ef5ea34cda18e
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-03-04 17:06:48 -0700

    fixed home routing

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ let fs = require('fs')
 http.createServer((req, res) => {
 	let q = url.parse(req.url, true);
 	let filename = `.${q.pathname}.html`
-	fs.readFile(filename, (err, data) => {  
-		if (err) {
-			fs.readFile("./404.html", (err, data) =>{
-				res.writeHead(404, {'Content-Type': 'text/html'})
-				res.write(data);
-				return res.end()
-			}); 
-		} else {
+
+	if (q.pathname === "/") {
+		fs.readFile("./index.html", (err, data) => {
 			res.writeHead(200, {'Content-Type': 'text/html'})
 			res.write(data);
 			return res.end();
-		}
-	})
+		})
+	} else {
+		fs.readFile(filename, (err, data) => {  
+			if (err) {
+				fs.readFile("./404.html", (err, data) =>{
+					res.writeHead(404, {'Content-Type': 'text/html'})
+					res.write(data);
+					return res.end()
+				}); 
+			} else {
+				res.writeHead(200, {'Content-Type': 'text/html'})
+				res.write(data);
+				return res.end();
+			}
+		})
+	}
+
 }).listen(8080);
